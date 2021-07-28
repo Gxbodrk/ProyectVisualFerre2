1cee6dab1ab93a8276e86eba9c2838a9f86efce1
JOSE RODOLFO RUIZ TIPAN <jose.ruiz.tipan@estudiantes.cordillera.edu.ec>
2021-07-27 21:42:09 -0500

    Login

---
+++ ProyectVisualFerre/Login.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectVisualFerre
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        //PANEL SUPERIOR
+        private void btn_min_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btn_maximizar_Click(object sender, EventArgs e)
+        {
+            //this.WindowState = FormWindowState.Maximized;
+            this.Size = Screen.PrimaryScreen.WorkingArea.Size;
+            this.Location = Screen.PrimaryScreen.WorkingArea.Location;
+            btn_maximizar.Visible = false;
+            btn_resta.Visible = true;
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_resta_Click(object sender, EventArgs e)
+        {
+            //this.WindowState = FormWindowState.Normal;
+            this.Size = new Size(1000, 600);
+            btn_resta.Visible = false;
+            btn_maximizar.Visible = true;
+        }
+    }
+}
