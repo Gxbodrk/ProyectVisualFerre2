9911dc8a29c8fadbcfedab21f86f0e42d37e1edb
GEOVANNY GABRIEL PILLAJO GONZALEZ <geovanny.pillajo.gonzalez@estudiantes.cordillera.edu.ec>
2021-07-27 21:04:28 -0500

    vista estadisticas

---
+++ ProyectVisualFerre/Admin_Estadisticas.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace ProyectVisualFerre
+{
+    partial class Admin_Estadisticas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(264, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ESTADÍSTICAS";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 164);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(340, 360);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(359, 164);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(313, 355);
+            this.panel2.TabIndex = 3;
+            // 
+            // Admin_Estadisticas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(764, 531);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Admin_Estadisticas";
+            this.Text = "Admin_Estadisticas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
---
+++ ProyectVisualFerre/Admin_Estadisticas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectVisualFerre
+{
+    public partial class Admin_Estadisticas : Form
+    {
+        public Admin_Estadisticas()
+        {
+            InitializeComponent();
+        }
+    }
+}
