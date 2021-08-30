27826add830dce65cacc92ad16b37c71b403735d
JOSE RODOLFO RUIZ TIPAN <jose.ruiz.tipan@estudiantes.cordillera.edu.ec>
2021-08-29 21:20:26 -0500

    Conexion a la base de datos

---
+++ ProyectVisualFerre/ConexionBD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectVisualFerre
+{
+    class ConexionBD
+    {
+        //Funcion para conectar la base de datos
+        public static SqlConnection Conectar()
+        {
+            //Hacemos el llamado a la conexion de la base de datos
+            SqlConnection con = new SqlConnection("Data Source=LoginMovilCB.mssql.somee.com;Initial Catalog=LoginMovilCB;Persist Security Info=True;User ID=joseruiz_SQLLogin_1;Password=v31usdbjx1");
+            con.Open();
+            return con;
+        }
+    }
+}
